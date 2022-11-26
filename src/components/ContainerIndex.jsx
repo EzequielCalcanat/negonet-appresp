1726b2911e9f8e5fe89c3bb4e9561d4334e5c729
EzequielCalcanat <79470817+EzequielCalcanat@users.noreply.github.com>
2022-11-25 20:53:25 -0600

    img profesión

--- src/components/ContainerIndex.jsx
+++ src/components/ContainerIndex.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import servicios from '../assets/imgs/servicios.png'
+import cocinero from '../assets/imgs/cocinero.jpg'
+import carpintero from '../assets/imgs/carpintero.jpg'
+import maestra from '../assets/imgs/maestra.jpg'
 
 const ContainerIndex = () => {
     return (
@@ -56,8 +59,7 @@ const ContainerIndex = () => {
             <div className='container-fluid containertext divinclinado text-center'>
                 <div className='row divtextinclinado'>
                     <div className="col-lg-4 contents">
-                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-
+                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns={cocinero} role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
                         <h2 className="fw-normal">Heading</h2>
                         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
                         <p><a className="btn btn-secondary" href="#">View details »</a></p>
